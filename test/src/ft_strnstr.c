91f3693114331a9afb3f69de5eba90a619115a04
Malesela CHOCHO <mchocho@c5r4s2.wethinkcode.co.za>
2019-05-30 18:04:44 +0200

    Included ft_strnstr.c

--- test/src/ft_strnstr.c
+++ test/src/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: mchocho <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/30 16:23:04 by mchocho           #+#    #+#             */
-/*   Updated: 2019/05/30 17:33:16 by mchocho          ###   ########.fr       */
+/*   Updated: 2019/05/30 18:03:29 by mchocho          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 char *ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-    	if ()!*ne    
+    	if (!*needle) return ((char *)haystack);   
 
 		size_t i;
 		int j;
@@ -22,28 +22,24 @@ char *ft_strnstr(const char *haystack, const char *needle, size_t len)
 		
 		i = 0;
         j = 0;
-        k = -1;
 
         while (haystack[i] != '\0' && i < len)
         {
                 if (haystack[i] == needle[j])
                 {
                         k = (int)i;
-                        while (haystack[k] == needle[j]) {
-                                if (needle[j++] == '\0') {
+                        while (haystack[k] == needle[j]) 
+						{
+                                if (needle[j + 1] == '\0')
                                         return ((char *) haystack + i);
-                                }
                                 j++;
                                 k++;
                         }
                 }
                 j = 0;
-                k = -1;
                 i++;
         }
-        if (!*needle) {
-                return ((char *)haystack);
-        } else if (i == 0) {
+        if (i == 0) {
                 return ((char *)(haystack + i));
         } else {
                 return (NULL);
@@ -57,14 +53,15 @@ int main()
 {
 	printf(" Testing ft_strnstr\n-------------------------\n");
 
-	char foo[] = "Every dog has his day.";
+	char foo[30] = "Every dog has his day.";
 
 	char *fubu = "dog";
 
+	int i = 20;
 
-	printf("Result should be: %s\n", strnstr(foo, fubu, 3));
+	printf("Result should be: %s\n", strnstr(foo, fubu, i));
 
-	printf("Test returned: %s\n", ft_strnstr(foo, fubu, 3));
+	printf("Test returned: %s\n", ft_strnstr(foo, fubu, i));
 
 	return 0;
 }
