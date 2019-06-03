13aba48ff2a5ba6df38e66239a4f2d0ec2e2b85a
Malesela CHOCHO <mchocho@c4r12s7.wethinkcode.co.za>
2019-06-03 18:57:51 +0200

    Included ft_strclr.c

---
+++ src/ft_strclr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strclr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mchocho <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/03 18:42:26 by mchocho           #+#    #+#             */
+/*   Updated: 2019/06/03 18:54:04 by mchocho          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void ft_strclr(char *s)
+{
+	while(*s)
+	{
+		*s = '\0';
+		s++;
+	}
+	return;
+}
+
+#include <stdio.h>
+
+int main() {
+	printf(" Testing ft_strclr.c\n-------------------------------\n");
+
+	char foo[50] = "Some long ass string";
+
+	printf("Result before ft_strclr: %s\n", foo);
+
+	ft_strclr(foo);
+
+	printf("Result returned: %s\n", foo);
+
+	return 0;
+}
