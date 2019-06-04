86f57942ae55bf84b755ae68968bedadd1259e89
Malesela CHOCHO <mchocho@c5r3s6.wethinkcode.co.za>
2019-06-04 14:55:54 +0200

    Included ft_strtrim.c

---
+++ src/ft_strtrim.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mchocho <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/04 14:24:12 by mchocho           #+#    #+#             */
+/*   Updated: 2019/06/04 14:46:36 by mchocho          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+static int ft_isspace(int c)
+{
+	return (c == ' ' || c == '\t' || c == '\n');
+}
+
+char *ft_strtrim(char const *s)
+{
+	char *str;
+	int fchar;
+	int lchar;
+	int lspace;
+	int i;
+	int k;
+	int j;
+
+	fchar = -1;
+	lchar = -1;
+	lspace = 0;
+	i = 0;
+	k = 0;
+	j = 0;
+
+	while(s[i]) {
+		if (!ft_isspace(s[i])) {
+			if(fchar == -1) fchar = i;
+			lchar = i;
+			j = lspace + (j + 1);
+			lspace = 0;
+		} else lspace++;
+
+		i++;
+	}
+
+	str = (char *)malloc(sizeof(char)*j);
+
+	if (!str) return (NULL);
+
+	i = 0;
+	j = 0;
+
+	while(s[i]) {
+		if (i >= fchar && i <= lchar)
+		{
+			str[j] = s[i];
+			j++;
+		}
+		i++;
+	}
+
+	return (str);
+}
+
+#include <stdio.h>
+
+int main() {
+
+	printf(" Testing ft_strtrim.c\n----------------------\n");
+
+	char *foo = "\t\t\t\t\t           Hello                   Bitch!         ";
+
+	printf("Result before trim: %s\n", foo);
+
+	char *fubu = ft_strtrim(foo);
+
+	printf("Result after trim: %s\n", fubu);
+
+	return 0;
+
+}
