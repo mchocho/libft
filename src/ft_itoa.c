3f3b8f599a247a7c28f77b48a262508c114124da
Malesela CHOCHO <mchocho@c5r3s6.wethinkcode.co.za>
2019-06-05 12:51:42 +0200

    Fixed ft_itoa.c

--- src/ft_itoa.c
+++ src/ft_itoa.c
@@ -6,11 +6,12 @@
 /*   By: mchocho <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/04 15:03:02 by mchocho           #+#    #+#             */
-/*   Updated: 2019/06/04 17:12:44 by mchocho          ###   ########.fr       */
+/*   Updated: 2019/06/05 12:50:06 by mchocho          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <stdio.h>
 
 char *ft_itoa(int n)
 {
@@ -19,8 +20,9 @@ char *ft_itoa(int n)
 	int base;
 	//int sign;
 	int i;
+	int len;
 
-	base = 1;
+	base = 0;		//Assume zero based
 	//sign = 1;
 	result = 0;
 	i = 0;
@@ -28,18 +30,23 @@ char *ft_itoa(int n)
 	temp = n;
 
 	//Check for 0
-	if (n == 0) return ((char *)'0');
+	//if (n == 0) return ((char *)'0');
 
 	//Check for sign
-	if (n < 0) base++;
+	if (n < 0) {
+		base++;
+		printf("Provided a negative value\n");
+	} else {
+		printf("Provided a positive value\n");
+	}
 
 	while(temp % 10) {
 		base++;
-		temp = temp % 10;
+		temp = temp / 10;
 	}
-	base++;
+	printf("Provided number has a base %d\n", base);
 
-	result = (char *)malloc(sizeof(char) * (base));
+	result = (char *)malloc(sizeof(char) * (base + 1));
 
 	if (n < 0) {
 		result[i] = '-';
@@ -47,29 +54,38 @@ char *ft_itoa(int n)
 	}
 
 	temp = n;
-	result[base] = '\0';
+	len = base;
 
-	while(base--) {
-		if (n < 0 && base == 1) break;
-		temp = temp % 10;
-		result[base] = (char)temp;
+	if (n < 0) temp = temp * -1;
+
+	//Check for 1 base values
+	if(temp >= -9 && temp <= 9) {
+		result[i] = temp + '0';
+	} else {
+		while(base-- /*!= 1*/)  {
+			if(n < 0 && base == 1) break;
+			result[base] = (temp % 10) + '0';
+			temp = temp / 10;
+		}
 	}
+
+	result[len] = '\0';
 
 	return (result);
 }
 
-#include <stdio.h>
+//#include <stdio.h>
 
 int main() {
 	printf(" Testing ft_itoa.c\n----------------------\n");
 
-	int n = 55578;
+	int n = 622898323;
 
-	char *str = ft_itoa(n);
+	//char str[50] = ft_itoa(n);
 
 	printf("Result before itoa: %d\n", n);
 
-	printf("Result after itoa: %s\n", str);
+	printf("Result after itoa: %s\n", ft_itoa(n));
 
 	return 0;
 }
