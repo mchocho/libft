d3b18ffecbae25bcb91f19ee5df92bbb32e215e6
Malesela CHOCHO <mchocho@c5r3s6.wethinkcode.co.za>
2019-06-05 16:33:40 +0200

    Fixed ft_itoa.c

--- src/ft_itoa.c
+++ src/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: mchocho <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/04 15:03:02 by mchocho           #+#    #+#             */
-/*   Updated: 2019/06/05 12:50:06 by mchocho          ###   ########.fr       */
+/*   Updated: 2019/06/05 16:33:12 by mchocho          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,19 +32,27 @@ char *ft_itoa(int n)
 	//Check for 0
 	//if (n == 0) return ((char *)'0');
 
+	if (n == -2147483648) return ("-2147483648");
+	else if (n == 2147483647) return ("2147483647");
+
 	//Check for sign
 	if (n < 0) {
 		base++;
+		temp = temp * -1;
 		printf("Provided a negative value\n");
 	} else {
 		printf("Provided a positive value\n");
 	}
 
-	while(temp % 10) {
+	//if (n < 0) temp = temp * -1;
+
+	while(temp) {
 		base++;
 		temp = temp / 10;
 	}
 	printf("Provided number has a base %d\n", base);
+
+	if (n == 0) base++;
 
 	result = (char *)malloc(sizeof(char) * (base + 1));
 
@@ -59,13 +67,16 @@ char *ft_itoa(int n)
 	if (n < 0) temp = temp * -1;
 
 	//Check for 1 base values
-	if(temp >= -9 && temp <= 9) {
-		result[i] = temp + '0';
+	if(n == 0) {
+		result[--base] = '0';
+		result[1] = '\0';
+		return (result);
 	} else {
-		while(base-- /*!= 1*/)  {
-			if(n < 0 && base == 1) break;
-			result[base] = (temp % 10) + '0';
+		while(base--)  {
+			/*if ((temp % 10) == 0) result[base] = '0';
+			else*/ result[base] = (temp % 10) + '0';
 			temp = temp / 10;
+			if (n < 0 && base == 1) break;
 		}
 	}
 
@@ -79,7 +90,7 @@ char *ft_itoa(int n)
 int main() {
 	printf(" Testing ft_itoa.c\n----------------------\n");
 
-	int n = 622898323;
+	int n = 2147483647;
 
 	//char str[50] = ft_itoa(n);
 
