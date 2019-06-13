c168f1950dba72ba137af6d5b450ab92530bcd74
sal'e <malachocho01@gmail.com>
2019-06-13 16:37:53 +0200

    Included ft_strsplit.c -- Requires fix

---
+++ test/src/ft_strsplit.c
@@ -0,0 +1,22 @@
+char **ft_strsplit(char const *s, char c)
+{
+	char **result;
+	char *value;
+	int ldelimiter;
+	int count;
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	count = 0;
+	ldelimiter = -1;
+
+	while(*s) {
+		if (*s == c && ldelimiter != -1) {
+			count;
+		}
+		*s++;
+	}
+
+}
