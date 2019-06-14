2e709c078d46b244614d64c9303760446b5ea58e
Malesela CHOCHO <mchocho@c4r12s11.wethinkcode.co.za>
2019-06-14 17:55:57 +0200

    ft_strsplit -- Requires fix

--- test/src/ft_strsplit.c
+++ test/src/ft_strsplit.c
@@ -9,44 +9,63 @@ char **ft_strsplit(char const *s, char c)
 	int fchar;
 	int i;
 	int j;
+	int k;
 
 	i = 0;
 	//fchar = 0;
 	count = 0;
 	ldelimiter = -1;
 	j = 0;
+	//k = 0;
 
 	while(*s) {
 		if (*s == c && ldelimiter != -1)
 			count++;
 		s++;
+		i++;		//Find length of string in case no delimiter is found 
 	}
-
-	if (count == 0) return (NULL);
+	//iif (count == 0) return (NULL);
 	
-	ldelimiter = 1;
-	result = (char **)malloc(sizeof(char) * count + 1);
-
-	while(s[i]) {
-		if (s[i] == c) {
-			j = i;
-			while(s[j] != c) j++;	//Count length of string until next delimiter
-			if (j > i) {	//A valid string was found
-				fchar = i + 1;
-				value = (char *)malloc(sizeof(char) * ((j - i) + 1));	//Allocate string space
-				while(fchar < j) {		//Loop through the valid substring
-					value[fchar] = s[j];
-					fchar++;
-				}
-				result = &value;	//Allocate string value to array
-				result++;
-				fchar = -1;
-			}
+	//ldelimiter = 1;
+	if (/*count == 0 ||*/ !(result = (char **)malloc(sizeof(char) * (count + 1))))
+		return (NULL);
+	else if (count == 0) {
+		if (!(result = (char **)malloc(sizeof(char) * (i + 1))))
+			return (NULL);
+		while(*s) {
+			value = (char *)s;
+			i++;
+			s++;
 		}
-		i++;
+		**result = *value;
+	} else {
+		
+		//ldelimiter = 1;
+	
+		while(s[i]) {
+			if (s[i] == c) {						//Delimiter found
+				j = i + 1;							//Mark the current index
+				while(s[j] != c) j++;				//Count length of string until next delimiter
+				if (j > (i + 1)) {					//A valid string was found
+					fchar = i + 1;					//Mark first valid position of string
+					k = 0;
+					if (!(value = (char *)malloc(sizeof(char) * ((j - i) + 1))))
+						return (NULL);	//Allocate string space
+					while(fchar < j) {		//Loop through the valid substring
+						value[k] = s[fchar];
+						fchar++;
+						k++;
+					}
+					**result = *value;	//Allocate string value to array
+					result++;
+					//fchar = -1;
+				}
+			}
+			i++;
+		}
 	}
 	result++;
-	result = '\0';
+	**result = '\0';
 	return (result);
 }
 
@@ -55,22 +74,27 @@ char **ft_strsplit(char const *s, char c)
 int main() {
 	printf(" Testing ft_strsplit.c\n-------------------------------\n");
 
-	char **res;
+	int i = 0;
+	int j = 0;
 
-	char *foo = "Hello world";
+	//char **res;
+	char *foo = "Hello world u are too God to me yo.";
 
-	char *fubu = "Hello again";
+	char d = ' ';
 
-	char d = " ";
+	char **res = ft_strsplit(foo, d);
+	
+	printf("Result of array will be: \n");
 
-	**res = &foo;
+	while(res[i] != '\0') {
+		while(res[i][j] != '\0') {
+			printf("Index %d with value: %c\n", j, res[i][j]);
+			j++;
+		}
+		i++;
+	}
 
-	res++;
-
-	**res = &fubu;
+	printf("End of ft_strsplit.c test\n");
 
 	return(0);
-
-	
-
 }
