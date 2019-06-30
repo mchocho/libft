37db44145f5e2ea7336315d39b79ef4576e7c721
Malesela CHOCHO <mchocho@c5r7s8.wethinkcode.co.za>
2019-06-30 10:51:54 +0200

    Included ft_lstnew.c

---
+++ ft_lstnew.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mchocho <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/29 12:59:23 by mchocho           #+#    #+#             */
+/*   Updated: 2019/06/29 13:10:53 by mchocho          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_initlist()
+{
+	t_list *list;
+
+	if (!(list = (t_list *)malloc(sizeof(t_list))))
+		return (NULL);
+
+	list->content = NULL;
+	list->content_size = 0;
+	list->next = NULL;
+
+	return (list);
+}
+
+t_list *ft_lstnew(void const *content, size_t content_size)
+{
+	t_list nlist;
+
+	nlist = ft_initlist();
+
+	if (nlist == NULL)
+		return (NULL);
+
+	if (*content)
+	{
+		ft_memcpy(nlist->content, content, content_size);
+		nlist->content_size = content_sizel
+	}
+
+	return (nlist);
+}
+
+#include <stdio.h>
+
+int main()
+{
+	printf(" Testing ft_lstnew.c\n--------------------------\n");
+
+	size_t len = 10;
+
+	char *content = "Hey there world! Wink Wink.";
+
+	t_list node = ft_lstnew(content, len);
+}
