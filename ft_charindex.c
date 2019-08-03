a92a6612b64eb00adfcd9e88bf5f93b39745b7bc
Malesela CHOCHO <mchocho@c5r7s8.wethinkcode.co.za>
2019-08-03 16:33:44 +0200

    Included ft_charindex.c

--- ft_charindex.c
+++ ft_charindex.c
@@ -6,7 +6,7 @@
 /*   By: mchocho <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/29 17:24:32 by mchocho           #+#    #+#             */
-/*   Updated: 2019/07/29 17:40:08 by mchocho          ###   ########.fr       */
+/*   Updated: 2019/08/03 16:13:18 by mchocho          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,21 +20,4 @@ int ft_charindex(char c)
 		return ((int)(c - 'a'));
 	else
 		return (c);
-}
-
-#include <stdio.h>
-
-int main()
-{
-	printf(" Testing ft_charindex.c\n-----------------------------\n");
-
-	printf("%c ---> %d\n", 'a', ft_charindex('a'));
-	printf("%c ---> %d\n", 'c', ft_charindex('c'));
-	printf("%c ---> %d\n", 'g', ft_charindex('g'));
-	printf("%c ---> %d\n", 'x', ft_charindex('x'));
-	printf("%c ---> %d\n", 'z', ft_charindex('z'));
-	printf("%c ---> %d\n", 'A', ft_charindex('A'));
-	printf("%c ---> %d\n", 'H', ft_charindex('H'));
-	printf("%c ---> %d\n", 'Z', ft_charindex('Z'));
-	return 0;
 }
